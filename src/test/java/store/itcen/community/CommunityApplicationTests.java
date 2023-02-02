4070fb9b66bd94bc57fffa7d5c3f0f48c1f4fda5
yeaygit <eulim9926@naver.com>
2023-02-02 16:41:44 +0900

    ê처음ê°개발환경세팅

---
+++ src/test/java/store/itcen/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package store.itcen.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
