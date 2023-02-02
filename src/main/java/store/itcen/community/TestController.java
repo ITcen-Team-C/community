6ad7a35cd76825bd0db4791b1052465efaad8f9e
backcoder <backcoder2@gmail.com>
2023-02-02 21:07:29 +0900

    #jquery추가_static디렉토리추가

--- src/main/java/store/itcen/community/TestController.java
+++ src/main/java/store/itcen/community/TestController.java
@@ -1,13 +1,15 @@
 package store.itcen.community;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class controller {
-
+public class TestController {
     @GetMapping("/test")
-    public String asdf() {
+    public String tester() {
         return "test";
     }
+
 }
