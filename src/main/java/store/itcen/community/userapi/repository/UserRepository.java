992e29dc516dea17526c260298bf6c41ac3a1dbb
hyeokinen <kkunh97@naver.com>
2023-02-07 15:20:14 +0900

    # jsp 수정 및 entity,dao수정

--- src/main/java/store/itcen/community/userapi/repository/UserRepository.java
+++ src/main/java/store/itcen/community/userapi/repository/UserRepository.java
@@ -1,18 +1,18 @@
-//package store.itcen.community.userapi.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import store.itcen.community.userapi.entity.UserEntity;
-//
-//import java.util.Optional;
-//
-//public interface UserRepository extends JpaRepository<UserEntity, String> {
-//
-//    Optional<UserEntity> findByEmail(String email);
-//
-//
-//    // 이메일로 회원 조회
-//    UserEntity findAllByEmail(String email);
-//
-//    // 이메일 중복 검사
-//    boolean existsByEmail(String email);
-//}
+package store.itcen.community.userapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.itcen.community.userapi.entity.UserEntity;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+
+    // 이메일로 회원 조회
+    UserEntity findAllByEmail(String email);
+
+    // 이메일 중복 검사
+    boolean existsByEmail(String email);
+}
